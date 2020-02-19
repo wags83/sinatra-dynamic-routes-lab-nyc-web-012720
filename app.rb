7b431390ddf6cb3ced42c1c4ffab5b01819a6bdb
wags83 <gpwagner@gmail.com>
2020-02-19 17:07:18 -0500

    Done.

--- app.rb
+++ app.rb
@@ -22,8 +22,14 @@ class App < Sinatra::Base
     answer
    end
 
+  # get '/say/:word1/:word2/:word3/:word4/:word5' do
+  #   "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  # end
+
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+    values = params.map(|key, value| value)
+    values.join(" ")
+    values += "."
   end
     
   get '/:operation/:number1/:number2' do
